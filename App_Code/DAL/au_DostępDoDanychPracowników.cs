d013696d590d95c5d36820da458ec85495959a5f
Punk <2morrow2late@rambler.ru>
2015-01-07 20:17:21 +0100

    ObslugaKlientow

--- App_Code/DAL/au_DostępDoDanychPracowników.cs
+++ App_Code/DAL/au_DostępDoDanychPracowników.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 /// <summary>
 /// Summary description for au_DostępDoDanychPracowników
 /// </summary>
@@ -14,13 +15,14 @@ public class au_DostępDoDanychPracowników
 		// TODO: Add constructor logic here
 		//
 	}
-    public void ZapiszDaneKontaktowe(string IdAspNetDB, string Imię,
+    public void ZapiszDaneKontaktowe(string IdASPNETDB, string Imię,
         string Nazwisko, string Dyspozycyjność, string Ulica, string NrDomuMieszkania, string KodPocztowy,
-        string Miejscowość, string Email, string NrTelefonu, string UwagiDodatkowe);
+        string Miejscowość, string Email, string NrTelefonu, string UwagiDodatkowe)
 {
     au_ListaPracownikówTableAdapters.PracownicyFirmyTabeAdapter
-        AdapterPracownikówFirmy = new au_ListaPracownikówTableAdapters.PracownicyFirmyTableAdapter();
-        AdapterPracownikówFirmy.Insert(IdAspNetDB, Imię, Nazwisko, Dyspozycyjność, Ulica, NrDomuMieszkania, KodPocztowy, Miejscowość,
+        AdapterPracownikówFirmy = new 
+            au_ListaPracownikówTableAdapters.PracownicyFirmyTableAdapter();
+        AdapterPracownikówFirmy.Insert(IdASPNETDB, Imię, Nazwisko, Dyspozycyjność, Ulica, NrDomuMieszkania, KodPocztowy, Miejscowość,
     Email, NrTelefonu, UwagiDodatkowe);
 }
 }
